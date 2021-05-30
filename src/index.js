913a2024e360e84f00dfad4f21de885d21e3d961
MarciaCastro09 <81990973+MarciaCastro09@users.noreply.github.com>
2021-05-30 18:36:53 +0100

    Add changed files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// Search Engine
+
 function locationSearch(city) {
   let apiKey = "8f9aad3b8a1db633c9b6d2d3308648b9";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
@@ -36,15 +38,15 @@ function showTemperature(response) {
 
   let date = document.querySelector("#current-date");
   date.innerHTML = formatDate(response.data.dt * 1000);
+
+  getForecast(response.data.coord);
 }
 
 function getLocation(position) {
-  let latitude = position.coords.latitude;
-  let logitude = position.coords.longitude;
   let apiKey = "8f9aad3b8a1db633c9b6d2d3308648b9";
-  let apIURL = `https://api.openweathermap.org/data/2.5/weather?&lat=${latitude}&lon=${logitude}&appid=${apiKey}&units=metric`;
-  axios.get(apIURL).then(showTemperature);
-  console.log(apIURL);
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?&lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiURL).then(showTemperature);
+  console.log(apiURL);
 }
 
 function goLocation(event) {
@@ -101,6 +103,8 @@ function enterLocation(event) {
 let form = document.querySelector("#enter-submit");
 form.addEventListener("click", enterLocation);
 
+//Unit Conversion
+
 function showFahrenheit(event) {
   event.preventDefault();
   let temperature = document.querySelector("#tempDay");
@@ -126,6 +130,79 @@ fahrenheitShow.addEventListener("click", showFahrenheit);
 
 let celsiusShow = document.querySelector("#celsiusTemp");
 celsiusShow.addEventListener("click", showCelsius);
+
+//Forecast
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+
+  response.data.daily.forEach(function (dayData, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+          <div class="card border-warning mb-3 card-warning" style="max-width: 20rem;">
+
+          <div class="card-header">
+            <strong><em>${formatDay(dayData.dt)}</em></strong>
+          </div>
+
+          <div class="card-body text-warning">
+            <h5 class="card-city">${dayData.weather[0].description}</h5>
+            <p class="card-text">
+              <img src="http://openweathermap.org/img/wn/${
+                dayData.weather[0].icon
+              }@2x.png" alt="City Temperature" class="temperature" />
+              <strong>${Math.round(dayData.temp.max)}°C | ${Math.round(
+          dayData.temp.min
+        )}ºC</strong>
+            </p>
+          </div>
+          </div>
+        `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+
+  forecastElement.innerHTML = forecastHTML;
+
+  console.log(forecastHTML);
+}
+
+//API integration
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "8f9aad3b8a1db633c9b6d2d3308648b9";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+getForecast({
+  lon: -6.7572,
+  lat: 41.8058,
+});
+
+enterLocation("Bragança");
 
 /*Week 3 Homework
 let weather = {
